852d7b4725204bc04db2de86d19e9f2b33fa4160
Lugaga <maureezgaga@gmail.com>
2019-06-10 15:03:01 +0300

    add python file

---
+++ circumference-of-a-circle.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+# We can generate a random number using the randint method
+
+r = random.randint(1, 100)
+
+print("\nThe radius Is =",r)
+    
+circumference = 2*3.14*r
+
+print("\nThe Circumference Is =",circumference)
+
